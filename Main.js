ec5b3c4e580adab10db7169dadb362c2563e82ee
Dankrs1 <krstndaniel24@gmail.com>
2021-02-02 09:13:33 -0500

    clone
    Co-authored-by: Hogan_Tyler <tylhog24@bergen.org>

--- Main.js
+++ Main.js
@@ -1,1 +1,33 @@
+grid = []
 
+function setup()
+{
+    createCanvas(500, 500);
+    grid = MakeArray(10, 10);
+}
+
+function draw()
+{
+    background(220);
+
+    
+}
+
+function MakeArray(w, h)
+{
+    var newGrid = [];
+
+    for (var i = 0; i < w; i++)
+    {
+        var tempGrid = [];
+
+        for (var j = 0; j < h; j++)
+        {
+            tempGrid.push([]);
+        }
+
+        newGrid.push(tempGrid);
+    }
+
+    return newGrid;
+}
